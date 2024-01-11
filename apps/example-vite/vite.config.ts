d2d0a25b96b1cb853b7f5b7dc576a46b989eadfd
Tsotne Gvadzabia <tsotne24@hotmail.com>
2024-01-11 22:03:49 +0000

    get it running

--- apps/example-vite/vite.config.ts
+++ apps/example-vite/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import vitePostHog from "vite-plugin-posthog";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), vitePostHog()],
+});
