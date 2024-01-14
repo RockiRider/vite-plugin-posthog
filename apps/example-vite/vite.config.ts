e3d5398523f87cdced0fd2b0995c740a589b1bd9
Tsotne Gvadzabia <tsotne24@hotmail.com>
2024-01-14 14:18:51 +0000

    edits

--- apps/example-vite/vite.config.ts
+++ apps/example-vite/vite.config.ts
@@ -3,12 +3,10 @@ import react from "@vitejs/plugin-react";
 import { VitePostHog } from "vite-plugin-posthog";
 
 // https://vitejs.dev/config/
-
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
   // import.meta.env.VITE_NAME available here with: process.env.VITE_NAME
-  // import.meta.env.VITE_PORT available here with: process.env.VITE_PORT
 
   return defineConfig({
     plugins: [
