c2567c9d91cc5eb14a2ad0f7acf24d6bb39986ab
Tsotne Gvadzabia <tsotne24@hotmail.com>
2024-01-16 22:37:06 +0000

    working

--- packages/vite-plugin-posthog/rollup.config.mjs
+++ packages/vite-plugin-posthog/rollup.config.mjs
@@ -1,24 +1,26 @@
 // rollup.config.mjs
 import { defineConfig } from "rollup";
-import typescript from "rollup-plugin-typescript2";
+import typescript from "@rollup/plugin-typescript";
 
 const config = defineConfig([
   {
     input: "src/core/index.ts",
     output: {
-      file: "dist/index.js",
+      dir: "dist",
       format: "es",
     },
-    plugins: [typescript()],
+    plugins: [typescript({ outDir: "dist", include: ["src/core/**/*"] })],
   },
   {
     input: "src/react/index.ts",
     output: {
       dir: "dist/react",
-      preserveModules: true,
       format: "es",
     },
-    plugins: [typescript()],
+    plugins: [
+      typescript({ outDir: "dist/react", include: ["src/react/**/*"] }),
+    ],
+    external: ["react", "react-dom"],
   },
   // Add configurations for other frameworks as needed
 ]);
