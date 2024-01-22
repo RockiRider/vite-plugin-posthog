03794b0cb7af42eb3fd674c2569b1eee7edd7e67
Tsotne Gvadzabia <tsotne24@hotmail.com>
2024-01-22 17:22:14 +0000

    initial copy

---
+++ packages/react-posthog-consent/rollup.config.mjs
@@ -0,0 +1,36 @@
+// rollup.config.mjs
+import { defineConfig } from "rollup";
+import typescript from "@rollup/plugin-typescript";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+// import copy from "rollup-plugin-copy";
+
+const config = defineConfig([
+  {
+    input: "src/core/index.ts",
+    output: {
+      dir: "dist/core",
+      format: "es",
+    },
+    plugins: [
+      typescript({
+        outDir: "dist/core",
+        include: ["src/core/**/*"],
+      }),
+    ],
+  },
+  {
+    input: "src/react/index.ts",
+    output: {
+      dir: "dist/react",
+      format: "es",
+    },
+    plugins: [
+      typescript({ outDir: "dist/react", include: ["src/react/**/*"] }),
+      nodeResolve(),
+    ],
+    external: ["react", "react-dom"],
+  },
+  // Add configurations for other frameworks as needed
+]);
+
+export default config;
