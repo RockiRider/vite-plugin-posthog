e4ab33b56a3ff023f6037964271801f52166b6d4
Tsotne Gvadzabia <32150283+RockiRider@users.noreply.github.com>
2024-01-28 20:36:44 +0000

    VPP - Changes (#16)

--- packages/vite-plugin-posthog/src/react/hooks/useFeatureFlagEnabled.ts
+++ packages/vite-plugin-posthog/src/react/hooks/useFeatureFlagEnabled.ts
@@ -1,6 +1,11 @@
 import { useEffect, useState } from "react";
 import { useVitePostHog } from "./useVitePostHog";
 
+/**
+ * A hook that returns whether a feature flag is enabled.
+ * @param flag - the feature flag to check
+ * @returns
+ */
 export function useFeatureFlagEnabled(flag: string): boolean | undefined {
   const client = useVitePostHog();
 
